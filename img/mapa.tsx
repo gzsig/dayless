8f4e86ae8eaa44d43d4331d90476d85fcfe8c9f2
Gabriel Zsigmond <gaazsig@gmail.com>
2019-11-07 14:37:10 -0300

    limpo

--- img/mapa.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="mapa" tilewidth="32" tileheight="32" tilecount="5" columns="5">
- <image source="tiles-1.png" width="160" height="32"/>
-</tileset>
--- mapa.js
+++ mapa.js
@@ -4,13 +4,14 @@
 // Essa não é a forma mais "prosissional" de fazer, mas é a mais simples :)
 
 // Vamos chamar a variável de game, para ficar igual ao sandbox!
-var game = new Phaser.Game(800, 600, Phaser.AUTO, "divJogo");
+var game = new Phaser.Game(1280, 720, Phaser.AUTO, "divJogo");
 
 var map;
 var tileset;
 var layer;
 var player;
 var facing = 'left';
+var anim = 'idle';
 var jumpTimer = 0;
 var cursors;
 var jumpButton;
@@ -45,11 +46,11 @@ function TelaInicial(game) {
   this.preload = function() {
     game.load.tilemap('level1', 'img/level1.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.image('tiles-1', 'img/tiles-1.png');
-    game.load.spritesheet('dude', 'img/dude.png', 32, 48);
-    game.load.spritesheet('droid', 'img/droid.png', 32, 32);
+    game.load.spritesheet('char', 'img/char.png', 64, 64);
+    game.load.spritesheet('poste', 'img/poste.png', 32, 32);
     game.load.image('starSmall', 'img/star.png');
     game.load.image('starBig', 'img/star2.png');
-    game.load.image('background', 'img/background2.png');
+    game.load.image('background', 'img/skyline_purple.png');
     game.load.image('posteluz', 'img/posteluz.png');
   };
 
@@ -59,7 +60,7 @@ function TelaInicial(game) {
 
     game.stage.backgroundColor = '#000000';
 
-    bg = game.add.tileSprite(0, 0, 800, 600, 'background');
+    bg = game.add.tileSprite(0, 0, 1280, 720, 'background');
     bg.fixedToCamera = true;
 
     map = game.add.tilemap('level1');
@@ -77,21 +78,27 @@ function TelaInicial(game) {
 
     game.physics.arcade.gravity.y = 1000;
 
-    player = game.add.sprite(32, 32, 'dude');
+    player = game.add.sprite(32, 32, 'char');
     game.physics.enable(player, Phaser.Physics.ARCADE);
 
     player.body.bounce.y = 0.2;
     player.body.collideWorldBounds = true;
-    player.body.setSize(20, 32, 5, 16);
+    player.body.setSize(20, 64, 22, 0);
 
-    player.animations.add('left', [0, 1, 2, 3], 10, true);
-    player.animations.add('turn', [4], 20, true);
-    player.animations.add('right', [5, 6, 7, 8], 10, true);
+    player.animations.add('left', [0, 1, 2, 3, 4], 10, true);
+    player.animations.add('idle-left', [0], 10, true);
+    player.animations.add('jump-left', [6], 10, true);
+    player.animations.add('right', [13, 12, 11, 10, 9], 10, true);
+    player.animations.add('idle-right', [13], 10, true);
+    player.animations.add('jump-right', [7], 10, true);
+    player.animations.play('idle-left');
+    facing = 'left';
+    anim = 'idle-left';
 
     game.camera.follow(player);
 
-    lamp = game.add.image(10, 10, 'posteluz');
-
+    lamp = game.add.sprite(20, 800, 'poste');
+    lamp.frame = 1;
 
 
     cursors = game.input.keyboard.createCursorKeys();
@@ -103,46 +110,56 @@ function TelaInicial(game) {
     game.physics.arcade.collide(player, layer);
 
     player.body.velocity.x = 0;
+    var onFloor = player.body.onFloor();
+    var newAnim = 'idle-left';
 
     if (cursors.left.isDown)
     {
         player.body.velocity.x = -250;
 
-        if (facing != 'left')
-        {
-            player.animations.play('left');
-            facing = 'left';
+        facing = 'left';
+        if (onFloor) {
+          newAnim = 'left';
+        } else {
+          newAnim = 'jump-left';
         }
     }
     else if (cursors.right.isDown)
     {
         player.body.velocity.x = 250;
 
-        if (facing != 'right')
-        {
-            player.animations.play('right');
-            facing = 'right';
+        facing = 'right';
+        if (onFloor) {
+          newAnim = 'right';
+        } else {
+          newAnim = 'jump-right';
         }
     }
     else
     {
-        if (facing != 'idle')
+        if (facing == 'left')
         {
-            player.animations.stop();
-
-            if (facing == 'left')
-            {
-                player.frame = 0;
-            }
-            else
-            {
-                player.frame = 5;
-            }
-
-            facing = 'idle';
+          if (onFloor) {
+            newAnim = 'idle-left';
+          } else {
+            newAnim = 'jump-left';
+          }
+        }
+        else
+        {
+          if (onFloor) {
+            newAnim = 'idle-right';
+          } else {
+            newAnim = 'jump-right';
+          }
         }
     }
     
+    if (anim != newAnim) {
+      anim = newAnim;
+      player.animations.play(anim);
+    }
+
     if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer)
     {
         player.body.velocity.y = -700;
